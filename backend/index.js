11d2c01bdad6baae536cd4be688bdc2d60601bf5
Anjan Saklani <akarshsajlan855@gmail.com>
2026-04-18 23:51:04 +0530

    new project

--- backend/index.js
+++ backend/index.js
@@ -1,23 +1,8 @@
-import express from'express'
-import cors from 'cors'
-import dotenv from 'dotenv'
+const app=require('./src/app.js')
+const dotenv=require('dotenv')
 dotenv.config()
-const app=express()
-app.use(cors())
-app.use(express.json())
-const PORT=process.env.PORT || 4000
-app.get('/api',(req,res)=>{
-    res.send("hello")
-})
+const PORT= process.env.PORT || 4000
 
-app.get('/api/home',(req,res)=>
-{
-  let arr=["anjan",23,89]
-  res.send(arr)
+app.listen(PORT, ()=>{
+   console.log(`app listening on http://localhost:${PORT}`)
 })
-
-app.listen(PORT , ()=>
-{
-    console.log(`SERVER RUNNING AT ${PORT}`)
-}
-)
